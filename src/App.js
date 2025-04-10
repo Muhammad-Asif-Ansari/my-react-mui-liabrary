97cdc24f192fd2e57f97d40678f8fde0e52c94fe
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2025-04-11 04:07:50 +0500

    mui library

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react"
 import BasicButtonGroup from "./components/Button/Button.js"
+import BasicPagination from "./components/Button/pagination/Pagination.js"
 const App = ()=>{
   return(
     <>
 
-    <div>
-  
+<div>
+<div>
+  <h1>Button</h1>
      <BasicButtonGroup/>
+     
     </div>
+    <div>
+      <h1>Pagination</h1>
+    <BasicPagination/>
+    </div>
+</div>
     </>
   )
 }
